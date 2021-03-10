acc26b3027c7005829b3ba30dfe3455c5774ed10
misurac <rmisurac@gmail.com>
2021-03-09 21:32:03 -0600

    Added an expense class and an AddExpensePage

---
+++ BudgetApp/BudgetApp/Expense.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetApp
+{
+    public enum ExpenseCategory
+    {
+        Housing,
+        Transit,
+        Groceries,
+        Exercise,
+        Travel,
+        Other
+    }
+    public class Expense
+    {
+
+        public string Name { get; set; }
+        public float Amount { get; set; }
+        public DateTime Date { get; set; }
+        public ExpenseCategory Category { get; set; }
+    }
+}
