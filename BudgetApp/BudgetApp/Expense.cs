fb55d08b1427ad4f4f896c188b283b2398e9bb43
misurac <rmisurac@gmail.com>
2021-03-10 10:15:13 -0600

    Added some basic UI

--- BudgetApp/BudgetApp/Expense.cs
+++ BudgetApp/BudgetApp/Expense.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace BudgetApp
@@ -13,12 +14,18 @@ namespace BudgetApp
         Travel,
         Other
     }
-    public class Expense
+    public class Expense /*: INotifyPropertyChanged*/
     {
 
         public string Name { get; set; }
         public float Amount { get; set; }
         public DateTime Date { get; set; }
+
+        //Research how to get selected date here: https://forums.xamarin.com/discussion/151957/how-to-save-selected-date-in-datepicker
+        //and here: https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.inotifypropertychanged?view=net-5.0
         public ExpenseCategory Category { get; set; }
+
+
+
     }
 }
