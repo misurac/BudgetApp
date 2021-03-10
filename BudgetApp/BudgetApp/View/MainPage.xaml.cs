ad13680419cbd38a805d3c3c1d3a34a29216d4bb
LalithaNadimpalli <lalli.nadimpalli@gmail.com>
2021-03-10 14:20:04 -0800

    MainPage Changes
    Mainpage changes

---
+++ BudgetApp/BudgetApp/View/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BudgetApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void AddNewExpense_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void ExpenseRecord_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
