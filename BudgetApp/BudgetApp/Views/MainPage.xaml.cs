32278de5aa8b6afa1573be09310ead8966ddfe70
misurac <rmisurac@gmail.com>
2021-03-10 20:49:29 -0600

    There are now working event handlers for each UI element

--- BudgetApp/BudgetApp/Views/MainPage.xaml.cs
+++ BudgetApp/BudgetApp/Views/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,35 @@ namespace BudgetApp
             InitializeComponent();
         }
 
+        //This is the event handler for the Category Picker
         private void SelectedCategory_SelectedIndexChanged(object sender, EventArgs e)
         {
+            var SelectedCategory = picker.Items[picker.SelectedIndex];
+            Debug.WriteLine(SelectedCategory);
+        }
 
+        //This is the event handler for the ExpenseDescription Entry box
+        private void ExpenseDescriptionTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+            Debug.WriteLine(newText);
+        }
+
+        //This is the event handler for the ExpenseAmount Entry box
+        private void ExpenseAmountTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+            Debug.WriteLine(newText);
+        }
+
+        //This is the event handler for the DatePicker
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            var picker = sender as DatePicker;
+            DateTime? date = picker.Date;
+            Debug.WriteLine(date);
         }
     }
 }
