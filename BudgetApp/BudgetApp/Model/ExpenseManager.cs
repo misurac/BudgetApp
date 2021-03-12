7b1764df0d9ed5f2cb262ddd3e212f0a9ca8e8a9
sushmakoosukuntla <78382542+sushmakoosukuntla@users.noreply.github.com>
2021-03-12 13:17:27 -0800

    ExpenseManager/Scratch
    Implemented ExpenseManager, SaveExpense and SaveBudget Methods

--- BudgetApp/BudgetApp/Model/ExpenseManager.cs
+++ BudgetApp/BudgetApp/Model/ExpenseManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Text;
 
 namespace BudgetApp.Model
@@ -9,22 +10,66 @@ namespace BudgetApp.Model
     {
         public static List<Expense> GetExpenses()
         {
-            // It is a placeholder method
-            //TODO read all the expenses stored in a folder and return to the UI
+            
+            
             var expenses = new List<Expense>();
-            expenses.Add(new Expense("Costco Groceries",78.50f,ExpenseCategory.Food));
-            expenses.Add(new Expense("House Rent", 450.00f, ExpenseCategory.Housing));
-            expenses.Add(new Expense("Wallgreens pharmacy", 18.50f, ExpenseCategory.Medical));
+            //Getting all .exp.txt files from specialFolder.
+            var files = Directory.EnumerateFiles(Environment.GetFolderPath
+                (Environment.SpecialFolder.LocalApplicationData), "*.exp.txt");
+            foreach (var filename in files)
+            {
+                var content = File.ReadAllText(filename);
+
+                /*Each filename content is a string seperated with commas. So, splitting with comma, and the
+                array will be store in splittedCOntent*/
+                var splittedContent = content.Split(',');
+                //coverting string in to enum. after converting, the value will be stored in myCategory.
+                Enum.TryParse(splittedContent[3], out ExpenseCategory myCategory);
+                //Parsing the parameters in to its type.
+                var expense = new Expense(splittedContent[0], float.Parse(splittedContent[1]),
+                    DateTime.Parse(splittedContent[2]), myCategory);
+                //Getting the file name for Each expense
+                expense.FileName = filename;
+                //Adding to expenses list
+                expenses.Add(expense);
+            }            
             return expenses;
         }
         /**
          * Returns true when expense is saved to a file
          * Returns false when something went wrong,UI can inform it to the user
          * */
+
+        //Will save Expense in to a file 
         public static bool SaveExpense(Expense e) {
-            //These are placeholders method
-            //TODO Save this expense to a file
+                    
+            if ((e != null) && (e.Amount > 0))
+            {
+                //converting the object in to string and saving it to the file.
+                var expenseProperties = e.ExpenseName + "," + e.Amount + "," + e.Date + "," + e.Category;
+                e.FileName = Path.Combine
+                    (Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    $"{Path.GetRandomFileName()}.exp.txt");
+                File.WriteAllText(e.FileName, expenseProperties);
+            }
             return true;
         } 
+
+        public static bool SaveBudget(float e)
+        {
+            if(e > 0)
+            {
+                var budget = e;
+                var budgetFilename = Path.Combine
+                    (Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    $"{Path.GetRandomFileName()}.bud.txt");
+                File.WriteAllText(budgetFilename, budget.ToString());
+            }
+            return true;
+        }
+            
     }
+
+
 }
+
