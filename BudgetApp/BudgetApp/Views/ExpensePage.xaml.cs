dd658041b03ae9df5c2484a04c177649ead95c28
misurac <rmisurac@gmail.com>
2021-03-13 13:26:13 -0600

    ExpensePage is populated with data from AddExpensePage test branch

--- BudgetApp/BudgetApp/Views/ExpensePage.xaml.cs
+++ BudgetApp/BudgetApp/Views/ExpensePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BudgetApp.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,14 +17,31 @@ namespace BudgetApp.Views
         public ExpensePage()
         {
             InitializeComponent();
+
+            //Here I create a list of type ExpenseCategory, which has the values of the Enum (i.e. 1, 2, 3, etc.)
+            ExpenseCategoriesList = new List<ExpenseCategory>();
+            ExpenseCategoriesList.Add(ExpenseCategory.Food);
+            ExpenseCategoriesList.Add(ExpenseCategory.Housing);
+            ExpenseCategoriesList.Add(ExpenseCategory.Insuarance);
+            ExpenseCategoriesList.Add(ExpenseCategory.Medical);
+            ExpenseCategoriesList.Add(ExpenseCategory.Other);
+            ExpenseCategoriesList.Add(ExpenseCategory.Saving);
+            ExpenseCategoriesList.Add(ExpenseCategory.Transportation);
+            ExpenseCategoriesList.Add(ExpenseCategory.Utilities);
+
+            //Here I convert the values of the Enum to the Names of the enum (i.e. Food, Housing, Insurance, etc.)
+            List<string> expenseCategoryStrings = ExpenseCategoriesList.ConvertAll(f => f.ToString());
+
+            //Here I set the picker to display the expenseCategoryStrings list
+            picker.ItemsSource = expenseCategoryStrings;
         }
 
-        //public string selectedCategory { get; set; }
+        public string selectedCategory { get; set; }
         public string expenseDescription { get; set; }
-        //public float expenseAmount { get; set; }
-        //public DateTime expenseDate { get; set; }
-        //public List<ExpenseCategory> ExpenseCategoriesList { get; set; }
-        //public List<string> expenseCategoryStrings { get; set; }
+        public float expenseAmount { get; set; }
+        public DateTime expenseDate { get; set; }
+        public List<ExpenseCategory> ExpenseCategoriesList { get; set; }
+        public List<string> expenseCategoryStrings { get; set; }
 
         //This is the event handler for the ExpenseDescription Entry box
         private void ExpenseDescriptionTextChanged(object sender, TextChangedEventArgs e)
@@ -32,5 +50,45 @@ namespace BudgetApp.Views
             var newText = e.NewTextValue;
             expenseDescription = newText;
         }
+
+        //This is the event handler for the Category Picker
+        private void SelectedCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //var SelectedCategory = picker.Items[picker.SelectedIndex];
+            var SelectedCategory = picker.Items[picker.SelectedIndex];
+            selectedCategory = SelectedCategory;
+            Debug.WriteLine(selectedCategory);
+        }
+
+        //This is the event handler for the ExpenseAmount Entry box
+        private void ExpenseAmountTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+            expenseAmount = float.Parse(newText);
+        }
+
+        //This is the event handler for the DatePicker
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            var picker = sender as DatePicker;
+            DateTime? date = picker.Date;
+            expenseDate = (DateTime)date;
+        }
+
+        //The event handler for the Save Button currently writes the values of the page to Output
+        private void SaveButtonClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine(selectedCategory);
+            Debug.WriteLine(expenseDescription);
+            Debug.WriteLine(expenseAmount);
+            Debug.WriteLine(expenseDate);
+        }
+
+        private void DeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
