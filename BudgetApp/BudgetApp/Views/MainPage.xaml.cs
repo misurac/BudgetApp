c7a6c2c7f90c168b82df1df64676e7cb2a71594e
LalithaNadimpalli <lalli.nadimpalli@gmail.com>
2021-03-12 16:39:43 -0800

    Mainpage and mainpage.cs
    xaml and .cs file

--- BudgetApp/BudgetApp/Views/MainPage.xaml.cs
+++ BudgetApp/BudgetApp/Views/MainPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using BudgetApp.Model;
+using BudgetApp.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,52 @@ namespace BudgetApp
         public MainPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            var expenses = new List<Expense>();
+            var records = Directory.EnumerateFiles(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "*.expense.txt");
+            foreach (var record in records)
+            {
+                var expense = new Expense
+                {
+                   
+                    
+                };
+                expenses.Add(expense);
+            }
+            ExpenseRecords.ItemsSource = expenses.OrderBy(n => n.Date).ToList();
+
+
+
+        }
+
+        private void HamburgerButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void AddNewExpense_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ExpensePage
+            {
+                BindingContext = new Expense()
+            });
+        }
+
+
+
+        private async void ExpenseRecord_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushModalAsync(new ExpensePage
+                {
+                    BindingContext = (Expense)e.SelectedItem
+                });
+            }
         }
     }
 }
