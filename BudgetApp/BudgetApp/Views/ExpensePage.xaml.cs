769ae82dc1b12cfca0610234ea218b8554eb1c67
misurac <rmisurac@gmail.com>
2021-03-13 11:58:36 -0600

    Adding features from ExpensePage

--- BudgetApp/BudgetApp/Views/ExpensePage.xaml.cs
+++ BudgetApp/BudgetApp/Views/ExpensePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BudgetApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,21 @@ namespace BudgetApp.Views
         public ExpensePage()
         {
             InitializeComponent();
+        }
+
+        //public string selectedCategory { get; set; }
+        public string expenseDescription { get; set; }
+        //public float expenseAmount { get; set; }
+        //public DateTime expenseDate { get; set; }
+        //public List<ExpenseCategory> ExpenseCategoriesList { get; set; }
+        //public List<string> expenseCategoryStrings { get; set; }
+
+        //This is the event handler for the ExpenseDescription Entry box
+        private void ExpenseDescriptionTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+            expenseDescription = newText;
         }
     }
 }
