fb51d14094d5930040cfda5c9650765e2a367327
sushmakoosukuntla <78382542+sushmakoosukuntla@users.noreply.github.com>
2021-03-16 13:01:24 -0700

    Update ExpensePage.xaml.cs

--- BudgetApp/BudgetApp/Views/ExpensePage.xaml.cs
+++ BudgetApp/BudgetApp/Views/ExpensePage.xaml.cs
@@ -46,54 +46,14 @@ namespace BudgetApp.Views
                 {
                     Description.Text = expense.ExpenseName;
                     ExpenseDate.Date = expense.Date;
-                    ExpenseAmount.Text = expense.Amount.ToString();
-                    //Picker= expense.Category.ToString();
-                }
+                    ExpenseAmount.Text = expense.Amount.ToString();                   
+                    //Picker.SelectedItemProperty= myCategory;
+                }  
             }
         }
-
-
-     //   public string selectedCategory { get; set; }
-    //    public string expenseDescription { get; set; }
-     //   public float expenseAmount { get; set; }
-    //    public DateTime expenseDate { get; set; }
         public List<ExpenseCategory> ExpenseCategoriesList { get; set; }
         public List<string> expenseCategoryStrings { get; set; }
-    //    public Expense currentExpense { get; set; }
-
-        //This is the event handler for the ExpenseDescription Entry box
-        /*private void ExpenseDescriptionTextChanged(object sender, TextChangedEventArgs e)
-        {
-            var oldText = e.OldTextValue;
-            var newText = e.NewTextValue;
-            expenseDescription = newText;
-        }
-
-        //This is the event handler for the Category Picker
-        private void SelectedCategory_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //var SelectedCategory = picker.Items[picker.SelectedIndex];
-            var SelectedCategory = picker.Items[picker.SelectedIndex];
-            selectedCategory = SelectedCategory;
-            Debug.WriteLine(selectedCategory);
-        }
-
-        //This is the event handler for the ExpenseAmount Entry box
-        private void ExpenseAmountTextChanged(object sender, TextChangedEventArgs e)
-        {
-            var oldText = e.OldTextValue;
-            var newText = e.NewTextValue;
-            expenseAmount = float.Parse(newText);
-        }
-
-        //This is the event handler for the DatePicker
-        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
-        {
-            var picker = sender as DatePicker;
-            DateTime? date = picker.Date;
-            expenseDate = (DateTime)date;
-        }*/
-
+    
         //The event handler for the Save Button currently writes the values of the page to Output
 
         private async void SaveButtonClicked(object sender, EventArgs e)
@@ -102,18 +62,7 @@ namespace BudgetApp.Views
             var expenseDate = ExpenseDate.Date;
             var expenseAmount = float.Parse(ExpenseAmount.Text);
             var category = (string)picker.SelectedItem;
-            Enum.TryParse(category, out ExpenseCategory myCategory);
-
-            /*//Picker.S = expense.Category.ToString();
-            ExpenseCategory PickedCategory = ExpenseCategory.Other;
-            //Finding the right ExpenseCategory to assign to currentExpense
-            for (int i = 0; i < expenseCategoryStrings.Count(); i++)
-            {
-                if (expenseCategoryStrings[i] == selectedCategory)
-                {
-                    PickedCategory = ExpenseCategoriesList[i];
-                }
-            }*/
+            Enum.TryParse(category, out ExpenseCategory myCategory);           
             //Creating a new instance of currentExpense
             Expense currentExpense = new Expense(expenseName, expenseAmount, expenseDate, myCategory);
             
